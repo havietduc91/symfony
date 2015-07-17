5ee88fb8b24d403f70b563745e10de52c88dfa5a
havietduc91 <havietduc91@gmail.com>
2015-07-17 17:10:26 +0700

    - Auto generate entities Entity/News.php - Show user by id, create user

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -2,15 +2,14 @@
 // src/AppBundle/Controller/BlogController.php
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\News;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class BlogController extends DefaultController
 {
-    /**
-     * @Route("/blog/{slug}", name="blog_show")
-     */
-    public function showAction($slug)
+    public function slugAction($slug)
     {
         echo 'Hello ' . $slug;
 
@@ -18,5 +17,40 @@ class BlogController extends DefaultController
             'article/show.html.twig',
             array('slug' => $slug)
         );
+    }
+
+    /**
+     * @Route("/blog/{slug}", name="blog_show")
+     */
+    public function showAction($id)
+    {
+
+        $news = $this->getDoctrine()
+            ->getRepository('AppBundle:News')
+            ->find($id);
+
+        if (!$news) {
+            throw $this->createNotFoundException(
+                'No news found for id '.$id
+            );
+        }
+
+        var_dump($news);
+        die();
+    }
+
+    public function createAction()
+    {
+        $news = new News();
+        $news->setName('news 2');
+        $news->setContent('Best news on this day');
+        $news->setDescription('Lorem ipsum dolor');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($news);
+        $em->flush();
+
+        return new Response('Created new id '.$news->getId());
     }
 }
--- src/AppBundle/Entity/News.php
+++ src/AppBundle/Entity/News.php
@@ -1,34 +1,116 @@
 <?php
 
+// src/AppBundle/Entity/New.php
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * News
- *
- * @ORM\Table()
  * @ORM\Entity
+ * @ORM\Table(name="new")
  */
 class News
 {
     /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer")
+     * @ORM\Column(type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="decimal", scale=2)
+     */
+    protected $content;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $description;
+
 
 
     /**
      * Get id
      *
-     * @return integer 
+     * @return integer
      */
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return News
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return News
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return News
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
     }
 }
